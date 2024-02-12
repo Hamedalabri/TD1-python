878403545ce9c4b12172131d11d243476fd69883
Hamedalabri <148535721+Hamedalabri@users.noreply.github.com>
2024-02-12 16:44:38 +0100

    Update find1.py

--- find1.py
+++ find1.py
@@ -1,4 +1,27 @@
-import sys
+from sys import argv
 import os 
+
+
+def affiche():
+     for i in os.listdir(argv[1]):
+          print(i)
+
+
+def main():
+     try:
+          if len(argv) == 1:
+               print("aucune argument n'a ete passé en parametre")
+
+          elif os.path.exists(argv[1]) == False:
+               print("le dossier n'eiste pas")
+          else:
+               affiche()
+     except Exception as e:
+          print("une erreur est survenue ", e )
+
+
+
+
 if __name__ == "__main__":
-    
+    main()
+     
